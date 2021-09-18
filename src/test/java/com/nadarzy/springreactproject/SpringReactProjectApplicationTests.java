f797d7f67e51a9ece678ab5c05050e4581663556
Julian Nadarzy <julian.nadarzy@gmail.com>
2021-09-18 14:11:36 +0200

    initial commit

---
+++ src/test/java/com/nadarzy/springreactproject/SpringReactProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nadarzy.springreactproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
